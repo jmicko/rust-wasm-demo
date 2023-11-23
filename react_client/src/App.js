76e4fcb08ab8fb4a63313dfd31fa93e47a845694
John Micko <jmicko@protonmail.com>
2023-11-23 12:07:17 -0600

    send js object into rs struct with serde

--- react_client/src/App.js
+++ react_client/src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import init, {add} from 'rust-wasm-demo';
+import init, {
+  add, double_age
+} from 'rust-wasm-demo';
 
 await init();
 
 function App() {
   const sum = add(5.1, 3.7);
   console.log('sum', sum);
+
+  // console.log('PersonTwo', PersonTwo);
+
+  // const person = new PersonTwo({name:'John', age: 30});
+  const person2 = {name:'John', age: 35};
+  console.log('person', person2);
+  console.log('person.age', person2.age);
+
+  const doubled_age = double_age(person2);
+  console.log('doubled_age', doubled_age);
+
+
   return (
     <div className="App">
       <header className="App-header">
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,7 @@
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
+// use wasm_bindgen::JsCast;
 // use js_sys::Object;
-// use serde::{Serialize, Deserialize};
+use serde::{Serialize, Deserialize};
 
 #[wasm_bindgen]
 pub fn add(a: f32, b: f32) -> f32 {
@@ -9,42 +9,58 @@ pub fn add(a: f32, b: f32) -> f32 {
 }
 
 // Structs
-#[wasm_bindgen]
+// #[wasm_bindgen]
+#[derive(Serialize, Deserialize)]
 pub struct PersonTwo {
-    name: String,
-    age: u8,
+    pub name: String,
+    pub age: u8,
+}
+
+// #[wasm_bindgen]
+#[derive(Serialize, Deserialize)]
+pub struct AgeDoubled {
+    pub name: String,
+    pub age: u8,
 }
 
 #[wasm_bindgen]
-pub struct age_doubled {
-    name: String,
-    age: u8,
-}
+pub fn double_age(val: JsValue) -> Result<JsValue, JsValue> {
+    // let mut field1 = HashMap::new();
+    // field1.insert(0, String::from("ex"));
+    let person: PersonTwo = serde_wasm_bindgen::from_value(val)?;
 
-#[wasm_bindgen]
-pub fn double_age(person: PersonTwo) -> age_doubled {
-    let person = age_doubled {
+    let newperson = AgeDoubled {
         age: person.age * 2,
         name: person.name,
     };
-    person
+
+    Ok(serde_wasm_bindgen::to_value(&newperson)?)
 }
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(typescript_type = "{ name: string }")]
-    pub type Person;
+// #[wasm_bindgen]
+// pub fn double_age(person: PersonTwo) -> AgeDoubled {
+//     let person = AgeDoubled {
+//         age: person.age * 2,
+//         name: person.name,
+//     };
+//     person
+// }
 
-    #[wasm_bindgen(method, getter)]
-    fn name(this: &Person) -> String;
-}
+// #[wasm_bindgen]
+// extern "C" {
+//     #[wasm_bindgen(typescript_type = "{ name: string }")]
+//     pub type Person;
 
-#[wasm_bindgen]
-pub fn greet(person: JsValue) -> Result<JsValue, JsValue> {
-    let person: Person = person.dyn_into().map_err(|_| JsValue::from_str("Expected a Person object"))?;
-    let greeting = format!("Hello, {}!", person.name());
-    Ok(JsValue::from_str(&greeting))
-}
+//     #[wasm_bindgen(method, getter)]
+//     fn name(this: &Person) -> String;
+// }
+
+// #[wasm_bindgen]
+// pub fn greet(person: JsValue) -> Result<JsValue, JsValue> {
+//     let person: Person = person.dyn_into().map_err(|_| JsValue::from_str("Expected a Person object"))?;
+//     let greeting = format!("Hello, {}!", person.name());
+//     Ok(JsValue::from_str(&greeting))
+// }
 
 // function that takes in a person object when called from JS
 // multiply the age by 2 and return the person object
